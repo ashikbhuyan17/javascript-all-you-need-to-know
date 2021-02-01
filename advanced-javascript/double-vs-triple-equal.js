bd6de74bd32220424757302d2371d08e9c956ae6
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2021-02-02 03:17:12 +0600

    double vs triple equal

---
+++ advanced-javascript/double-vs-triple-equal.js
@@ -0,0 +1,26 @@
+var c = '50'
+var d = 50
+//  triple Equals ( === ) checks for value and data type equality, 
+//  Double Equals ( == ) checks for value equality only.
+console.log('triple Equals : ', c === d)
+console.log('Double Equals : ', c == d);
+console.log(c !== d)
+
+
+// Double Equals 
+const num1 = 1
+const num2 = true
+if (num1 == num2) {
+    console.log('condition is true');
+} else {
+    console.log('condition is false');
+}
+
+// triple Equals
+const num3 = 1
+const num4 = true
+if (num3 === num4) {
+    console.log('condition is true');
+} else {
+    console.log('condition is false');
+}
