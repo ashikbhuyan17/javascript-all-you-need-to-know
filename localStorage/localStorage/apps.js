81113c27ba863c4ae4a1d6d8c95f9f31485bbbbd
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2021-02-19 02:57:33 +0600

    19/02/2021

---
+++ localStorage/localStorage/apps.js
@@ -0,0 +1,27 @@
+// setItem
+localStorage.setItem("name", "Ashik Bhuyan")
+localStorage.setItem("age", 22)
+
+// or shortCut
+localStorage.age = 85
+localStorage.bloodGroup = "b+ blood"
+
+// getItem
+// const fullName = localStorage.getItem("name")
+// console.log(fullName);
+// const age = localStorage.getItem("age")
+// console.log(age);
+// or shortCut 
+const fullName = localStorage.name
+console.log(fullName);
+const age = localStorage.age
+console.log(age);
+
+// remove
+// localStorage.removeItem("name")
+
+// clear
+// localStorage.clear()
+
+// key(index)
+// console.log(localStorage.key(1));
