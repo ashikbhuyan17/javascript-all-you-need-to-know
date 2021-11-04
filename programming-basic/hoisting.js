21212cfbefc61d4d39e52cad6bef106724269c54
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2021-11-04 20:32:52 +0600

    hoisting

---
+++ programming-basic/hoisting.js
@@ -0,0 +1,20 @@
+// JavaScript Declarations are Hoisted
+// In JavaScript, a variable can be declared after it has been used.
+
+// In other words; a variable can be used before it has been declared.
+
+x = 10
+console.log("x value is ", x)
+var x;
+
+
+
+var y;
+y = 20
+console.log("ÿ value is ", y)
+
+
+console.log('Value: ' + a);
+var a = 10;
+
+
