d3a28a4afa3ec1a07f63d143798bc04ee2bcb435
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2022-06-08 01:07:25 +0600

    variable

---
+++ brain-station/B3-class-1/index.js
@@ -0,0 +1,22 @@
+// variable => container => jekhane ami data gula rakthe pari
+// ex: table er upor mobile raklam tkn table ekta variable
+
+
+// page refresh diccci => javascript code run hoi => Execute hoi
+// execute => ekta kaj ke complete korake bujay (1.code ke line by line read kore 2.)
+
+
+/*  
+Execution Context => js code ke run korle Execution Context create hoi, eta dekhte onekta borgher moto
+jokhon kono javascript code run korbo imedate or sathe sathe  ekta borgho
+create hoi tokon etake Execution Context bole
+Execution Context :
+    # part
+        1. Memory Component or variable environment => variable gula takhe
+        2. Code Component or thread of execution
+    #phase(dap): manush 1st child,young,old,die   
+        1) Creation phase or Memory Creation phase -> variable create hoi
+        2) Execution phase  or Code Execution phase  -> line by line code execute kore
+
+
+*/
